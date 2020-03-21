0199a66a658b801d80d426f0a2e7056cda7bba64
Kevin Minutti <minutti70@gmail.com>
2020-03-21 20:27:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function  theBeatlesPlay(musicians, instruments) {
   var newArray = []
   let musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
   let instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
-for (let i = 0; i < musicians.length; i++ ){
+for (let i = 0; i < musicians.length && i < instruments.length; i++ ){
   newArray.push(`${musicians[i]} plays ${instruments[i]}`)
 }
  return newArray
@@ -14,7 +14,7 @@ for (let i = 0; i < musicians.length; i++ ){
 // * Use a `for loop`, which iterates over the array of musicians. Be careful
 //   about what value you set your `counter` variable to store. (Hint: Think
 //   about what the first index of an array is).
-// 
+//
 // * The first time through the loop, it should create a string using the _first_
 //   index of the musicians array **and** the first index of the instruments
 //   array: `"John Lennon plays guitar"`.
