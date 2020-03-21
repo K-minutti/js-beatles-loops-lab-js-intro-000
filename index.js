bc373c4241a85179d1dbf67a3bea66fd78361f80
Kevin Minutti <minutti70@gmail.com>
2020-03-21 20:51:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,23 @@ function  johnLennonFacts(array) {
 
 
 
-function iLoveTheBeatles() {
-
+function iLoveTheBeatles(number) {
+  var nwArray = []
+   do { 
+     nwArray.push("I love the Beatles!")
+     number ++
+   } while(nwArray < 15)
+return nwArray
 }
+
+* The function should create a variable that stores an empty array. 
+* Implement a do-while loop inside the function that adds `"I love the
+  Beatles!"` to the empty array. 
+* The loop should then increment the number passed in as a parameter. The
+  condition of the loop should check to see that the parameter number is
+  less than `15`. It should contain a number of strings that represents the
+  difference between the input number and `15`.
+* Return the array with the strings `"I love the Beatles!"`
+
+
+
