6c4702a9960a937f1f6bd24da09986bd8fb837f5
Kevin Minutti <minutti70@gmail.com>
2020-03-21 20:42:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,22 +9,25 @@ for (let i = 0; i < musicians.length && i < instruments.length; i++ ){
 }
 
 
-// * Use a `for loop`, which iterates over the array of musicians. Be careful
-//   about what value you set your `counter` variable to store. (Hint: Think
-//   about what the first index of an array is).
-//
-// * The first time through the loop, it should create a string using the _first_
-//   index of the musicians array **and** the first index of the instruments
-//   array: `"John Lennon plays guitar"`.
-// * Add this string to the empty array you created. The loop should make the
-//   same sentence for every member of the musicians array.
-
-
-
-
-function  johnLennonFacts() {
-
+function  johnLennonFacts(array) {
+ while(counter < array.length) {
+   array.each(x) x.join("!!!")
+   counter += 1
+ }
+ return array
 }
+let num = 1
+ 
+while (num < 6) {
+  console.log(num)
+  num += 1
+}
+
+* Use a while loop to loop over the facts array and add `"!!!"` to the end of
+every fact.
+* Return an array of strings with exclamation points.
+
+
 
 function iLoveTheBeatles() {
 
