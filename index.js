645da00858b732920581dc5eef251956dfbfe53d
Kevin Minutti <minutti70@gmail.com>
2020-03-21 20:10:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // add solution here
 
 function  theBeatlesPlay() {
-
+var playerInstruments["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar",
+"George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]
 }
 
 function  johnLennonFacts() {
