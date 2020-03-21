63501536db53297eb33ab4c22f82f82ecdff10cd
Kevin Minutti <minutti70@gmail.com>
2020-03-21 20:26:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ for (let i = 0; i < musicians.length; i++ ){
 }
 
 
-* Use a `for loop`, which iterates over the array of musicians. Be careful
-  about what value you set your `counter` variable to store. (Hint: Think
-  about what the first index of an array is).
-
-* The first time through the loop, it should create a string using the _first_
-  index of the musicians array **and** the first index of the instruments
-  array: `"John Lennon plays guitar"`.
-* Add this string to the empty array you created. The loop should make the
-  same sentence for every member of the musicians array.
+// * Use a `for loop`, which iterates over the array of musicians. Be careful
+//   about what value you set your `counter` variable to store. (Hint: Think
+//   about what the first index of an array is).
+// 
+// * The first time through the loop, it should create a string using the _first_
+//   index of the musicians array **and** the first index of the instruments
+//   array: `"John Lennon plays guitar"`.
+// * Add this string to the empty array you created. The loop should make the
+//   same sentence for every member of the musicians array.
 
 
 
