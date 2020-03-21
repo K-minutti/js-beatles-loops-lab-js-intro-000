7d36be4ab395b96bc12d2a71f068b317abfe9c98
Kevin Minutti <minutti70@gmail.com>
2020-03-21 20:26:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 // add solution here
 
-function  theBeatlesPlay() {
-var playerAndInstruments = ["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar",
-"George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]
- return playerAndInstruments
+function  theBeatlesPlay(musicians, instruments) {
+  var newArray = []
+  let musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+  let instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+for (let i = 0; i < musicians.length; i++ ){
+  newArray.push(`${musicians[i]} plays ${instruments[i]}`)
 }
+ return newArray
+}
+
+
+* Use a `for loop`, which iterates over the array of musicians. Be careful
+  about what value you set your `counter` variable to store. (Hint: Think
+  about what the first index of an array is).
+
+* The first time through the loop, it should create a string using the _first_
+  index of the musicians array **and** the first index of the instruments
+  array: `"John Lennon plays guitar"`.
+* Add this string to the empty array you created. The loop should make the
+  same sentence for every member of the musicians array.
+
+
+
 
 function  johnLennonFacts() {
 
