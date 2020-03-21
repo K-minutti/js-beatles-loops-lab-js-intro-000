1a1efd8cb445f69134d761cfa92fbc94a74b0a97
Kevin Minutti <minutti70@gmail.com>
2020-03-21 20:44:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ for (let i = 0; i < musicians.length && i < instruments.length; i++ ){
 function  johnLennonFacts(array) {
   let counter = 0
  while(counter < array.length) {
-   array.each.join("!!!")
+   array.push.join("!!!")
    counter += 1
  }
  return array
